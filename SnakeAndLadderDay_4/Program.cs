29f52595e546c2401349ebbce464d27b41b1befc
Subhadeep <subhadeep0326@gmail.com>
2021-10-10 16:25:44 +0530

    [subhadeep dir roll  UC2]

--- SnakeAndLadderDay_4/Program.cs
+++ SnakeAndLadderDay_4/Program.cs
@@ -10,9 +10,14 @@ namespace SnakeAndLadderDay_4
             Console.WriteLine();
             Console.WriteLine("Single Player mode: Starting position 0");  //as per UC1
             Console.WriteLine("....Game Begins....");
+            
+            //Variables
             int position = 0;
-            int dice;
-        
+
+            Random die = new Random();
+            int dice = die.Next(1, 7);
+            Console.WriteLine("The number on this die roll is: "+dice);
+            
         }
     }
 }
