31eaf7cb02cf43ccf10a66a397e0820f3390b7b3
Subhadeep <subhadeep0326@gmail.com>
2021-10-10 16:13:41 +0530

    [Subhadeep UC1]

--- SnakeAndLadderDay_4/Program.cs
+++ SnakeAndLadderDay_4/Program.cs
@@ -7,6 +7,12 @@ namespace SnakeAndLadderDay_4
         static void Main(string[] args)
         {
             Console.WriteLine("**Welcome to the game of snake and ladder**");
+            Console.WriteLine();
+            Console.WriteLine("Single Player mode: Starting position 0");  //as per UC1
+            Console.WriteLine("....Game Begins....");
+            int position = 0;
+            int dice;
+        
         }
     }
 }
