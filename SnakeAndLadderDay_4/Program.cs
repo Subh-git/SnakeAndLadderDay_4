e0e4891ee61a0bb696d1281d71ac293f0b9347ab
Subhadeep <subhadeep0326@gmail.com>
2021-10-10 16:04:47 +0530

    [Subhadeep Snake and ladder welcome message]

---
+++ SnakeAndLadderDay_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadderDay_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**Welcome to the game of snake and ladder**");
+        }
+    }
+}
